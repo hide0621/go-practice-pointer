2c287da9a1813689f95425c8a32d0982ff080753
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-11-27 17:41:13 +0900

    6th commit

--- main.go
+++ main.go
@@ -2,12 +2,28 @@ package main
 
 import "fmt"
 
+type Hoge struct {
+	value int
+}
+
+// ポインタ型ではなく値型なので、h.valueを書き換えられない
+func (h Hoge) HogeA(v int) {
+	h.value = v + 5
+}
+
+// ポインタ型なので、h.valueを書き換えられる
+func (h *Hoge) HogeB(v int) {
+	h.value = v + 5
+}
+
 func main() {
+	var h Hoge
 
-	var hoge string = "テストです"
-	var pointer *string = &hoge
+	h.HogeA(0)
+	// 0(ゼロ値)が出力される
+	fmt.Printf("HogeA(ポインタ型ではない)の出力結果:%v\n", h.value)
 
-	// "テストです"
-	fmt.Println(*pointer)
-
+	h.HogeB(0)
+	// +5された値が出力される
+	fmt.Printf("HogeB(ポインタ型である)の出力結果:%v\n", h.value)
 }
