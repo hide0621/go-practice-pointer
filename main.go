5d66a64f18476b865369bec66b9d7a2393963598
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-11-30 15:52:56 +0900

    mapの戻り値を値型にしたい場合の書き方

--- main.go
+++ main.go
@@ -9,21 +9,37 @@ type Food struct {
 }
 
 func main() {
+	fmt.Println(MakeResult())           //map[うまいもの:0x14000106270]
 	fmt.Println(MakeResult()["うまいもの"])  //&{1 焼肉 すこぶるうまい}
 	fmt.Println(*MakeResult()["うまいもの"]) //{1 焼肉 すこぶるうまい}
+
+	x := MakeResult()
+	x["うまいもの"].Description = "hoge"
+	fmt.Println(x)                      //map[うまいもの:0x14000112300]
+	fmt.Println(x["うまいもの"].ID)          //1
+	fmt.Println(x["うまいもの"].Name)        //焼肉
+	fmt.Println(x["うまいもの"].Description) //hoge
+
+	y := MakeResult1()
+	//エラー：cannot assign to struct field y["うまいもの"].Description in map
+	// y["うまいもの"].Description = "fuga"
+
+	fmt.Println(y) //map[うまいもの:{1 焼肉 すこぶるうまい}]
 }
 
 // エラー：cannot assign to struct field result["うまいもの"].Description in map
-// func MakeResult() map[string]Food {
-// 	result := make(map[string]Food)
-// 	result["うまいもの"] = Food{
-// 		ID:   1,
-// 		Name: "焼肉",
-// 	}
-// 	result["うまいもの"].Description = "すこぶるうまい"
+/*
+func MakeResult() map[string]Food {
+	result := make(map[string]Food)
+	result["うまいもの"] = Food{
+		ID:   1,
+		Name: "焼肉",
+	}
+	result["うまいもの"].Description = "すこぶるうまい"
 
-// 	return result
-// }
+	return result
+}
+*/
 
 func MakeResult() map[string]*Food {
 	result := make(map[string]*Food)
@@ -33,5 +49,20 @@ func MakeResult() map[string]*Food {
 	}
 	result["うまいもの"].Description = "すこぶるうまい"
 
+	return result
+}
+
+func MakeResult1() map[string]Food {
+	pointerResult := make(map[string]*Food)
+	pointerResult["うまいもの"] = &Food{
+		ID:   1,
+		Name: "焼肉",
+	}
+	pointerResult["うまいもの"].Description = "すこぶるうまい"
+
+	result := make(map[string]Food)
+	for k, v := range pointerResult {
+		result[k] = *v
+	}
 	return result
 }
