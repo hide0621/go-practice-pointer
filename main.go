b9fa7930f6628ca6035c20c14d6123c0a666fa08
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-12-01 10:07:57 +0900

    スライスの要素を追加した場合のアドレスの変化について

--- main.go
+++ main.go
@@ -5,24 +5,19 @@ import (
 )
 
 func main() {
-	i := 1
-	fmt.Printf("address is %v\n", &i) //0x14000126008
-	updateArgInt(i)                   //この段階の実引数iは上記の変数iと同じアドレス？
-	fmt.Printf("引数がint型の時、サブ関数の処理後、iの値は%v\n", i)
-	updateArgPointer(&i)
-	fmt.Printf("引数が*int型の時、サブ関数の処理後、iの値は%v\n", i)
+	s := []int{1, 2, 3}
+
+	fmt.Printf("最初の変数s:%v, 最初の変数sのアドレス:%v\n", s, &s[1]) //最初の変数s:[1 2 3] 最初の変数sのアドレス:0x1400012c020
+	updateArgSlice(s)
+	fmt.Printf("サブ関数処理後の変数s:%v\n", s) //サブ関数処理後の変数s:[1 2 3]
 }
 
-// 引数iのアドレス：0x14000126020
-// 値渡しなのでサブ関数のスコープ内でしかインクリメントされない
-func updateArgInt(i int) {
-	fmt.Printf("address is %v\n", &i)
-	i++
-}
+func updateArgSlice(s []int) {
+	fmt.Printf("要素追加前のアドレス:%v\n", &s[1]) //素追加前のアドレス:0x1400012c020
 
-// 引数iに渡ったアドレス：0x14000126008
-// 参照渡しなのでサブ関数を越えて影響する
-func updateArgPointer(i *int) {
-	fmt.Printf("address is %v\n", i)
-	*i++
+	//要素を追加した時点で配列のサイズが変わり、「配列のサイズ」という型も変わる
+	//よってこの配列を参照するスライスのアドレスも変わるので、メイン関数内には影響を及ぼせない
+	s = append(s, 4)
+	fmt.Printf("要素追加後のアドレス:%v\n", &s[1]) //要素追加後のアドレス:0x14000132008
+	s[0] = 10
 }
